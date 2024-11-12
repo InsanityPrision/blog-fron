40ab5f1049a923c6158bf3a56fea6f4d4dbd4f18
Erik Riquelme <erikone2311@gmail.com>
2024-11-12 19:59:01 +0100

    Fix centered header and app component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,9 @@ import "./Header.css";
 const Header: React.FC = () => {
   return (
     <header className="main-header">
-      <h1>Blog</h1>
+      <div className="main-header__container">
+        <h1 className="main-title">Blog</h1>
+      </div>
     </header>
   );
 };
