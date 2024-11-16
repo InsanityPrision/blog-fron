82b487809da2a67b4da74cfbbb4a6a892ac08247
Erik Riquelme <erikone2311@gmail.com>
2024-11-16 22:38:30 +0100

    Add content in BlogPage and request posts

--- src/pages/BlogPage/BlogPage.tsx
+++ src/pages/BlogPage/BlogPage.tsx
@@ -1,5 +1,36 @@
+import { useCallback, useEffect, useState } from "react";
+import PostsList from "../../post/components/PostsList/PostsList";
+import { Post } from "../../post/types";
+import "./BlogPage.css";
+import getPosts from "../../client/getPosts";
+
 const BlogPage: React.FC = () => {
-  return <></>;
+  const [postsApi, setPostsApi] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadPosts = useCallback(async () => {
+    const { posts } = await getPosts();
+
+    setPostsApi(posts);
+  }, []);
+
+  useEffect(() => {
+    setIsLoading(true);
+    loadPosts();
+    setIsLoading(false);
+  }, [loadPosts]);
+
+  return (
+    <main className="main-content">
+      <h2>Posts:</h2>
+      <PostsList posts={postsApi} />
+      {isLoading && (
+        <div className="loader-container">
+          <span className="loader"></span>
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default BlogPage;
