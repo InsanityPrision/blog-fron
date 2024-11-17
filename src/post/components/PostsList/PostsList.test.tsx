6fe658fee94b66b818f9b9fb173563399a65fe3b
Erik Riquelme <erikone2311@gmail.com>
2024-11-17 22:24:00 +0100

    Add PostsList test

---
+++ src/post/components/PostsList/PostsList.test.tsx
@@ -0,0 +1,48 @@
+import { MemoryRouter } from "react-router-dom";
+import PostsList from "./PostsList";
+import { render, screen } from "@testing-library/react";
+import { Post } from "../../types";
+
+describe("Given the PostList component", () => {
+  describe("When it receives 'I love cats' and 'I have cats' posts", () => {
+    test("Then it should show 'I love cats' and 'I have cats' in a heading", async () => {
+      const postLoveCatsTitle = /I like cats/i;
+      const postHateCatsTitle = /I hate cats/i;
+      const posts: Post[] = [
+        {
+          title: "I like cats",
+          imageUrl: "",
+          content: "I like cats forever!",
+          author: "LoverCats04",
+          id: "cb0cb815-0673-499c-bb94-beed32496f32",
+          date: "2024-11-16T16:18:17.501Z",
+        },
+        {
+          title: "I hate cats",
+          imageUrl: "",
+          content: "I hate cats forever!",
+          author: "HaterCats04",
+          id: "0731f668-592c-49f8-a0bb-a2ac9b8d72f1",
+          date: "2024-11-16T16:18:17.501Z",
+        },
+      ];
+
+      render(
+        <MemoryRouter>
+          <PostsList posts={posts} />
+        </MemoryRouter>,
+      );
+
+      const postILoveCats = screen.getByRole("heading", {
+        name: postLoveCatsTitle,
+      });
+
+      const postIHateCats = screen.getByRole("heading", {
+        name: postHateCatsTitle,
+      });
+
+      expect(postILoveCats).toBeInTheDocument();
+      expect(postIHateCats).toBeInTheDocument();
+    });
+  });
+});
