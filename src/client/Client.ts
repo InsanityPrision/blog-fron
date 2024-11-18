3762dd033c8d27d68b155fd8885059b304c4838b
Erik Riquelme <erikone2311@gmail.com>
2024-11-18 23:50:26 +0100

    Change postPost to createPost to a method

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,4 +1,4 @@
-import { Post } from "../post/types";
+import { Post, PostData } from "../post/types";
 import { ClientStructure } from "./types";
 
 class Client implements ClientStructure {
@@ -10,6 +10,18 @@ class Client implements ClientStructure {
     const posts = (await response.json()) as { posts: Post[] };
 
     return posts;
+  };
+
+  createPost = (postData: PostData): void => {
+    const url = import.meta.env.VITE_API_URL;
+
+    fetch(`${url}/posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    });
   };
 }
 
