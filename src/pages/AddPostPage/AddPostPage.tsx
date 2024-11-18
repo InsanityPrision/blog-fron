4c768727b59aaecddd210704b50e327e13b04eb9
Erik Riquelme <erikone2311@gmail.com>
2024-11-18 01:59:28 +0100

    Move form into a new component

--- src/pages/AddPostPage/AddPostPage.tsx
+++ src/pages/AddPostPage/AddPostPage.tsx
@@ -1,21 +1,11 @@
-import Button from "../../components/Button/Button";
+import PostForm from "../../post/components/PostForm/PostForm";
 import "./AddPostPage.css";
 
 const AddPost: React.FC = () => {
   return (
     <main className="formPage--container">
       <h2>Add a new post:</h2>
-      <form className="form">
-        <label htmlFor="title">Title:</label>
-        <input className="form__input" type="text" id="title" required />
-        <label htmlFor="content">Content:</label>
-        <input type="text" className="form__input" id="content" required />
-        <label htmlFor="imageUrl">Image URL:</label>
-        <input type="url" className="form__input" id="imageUrl" required />
-        <label htmlFor="author">Author:</label>
-        <input type="text" className="form__input" id="author" required />
-        <Button type="submit" text="Create post"></Button>
-      </form>
+      <PostForm />
     </main>
   );
 };
---
+++ src/post/components/PostForm/PostForm.tsx
@@ -0,0 +1,19 @@
+import Button from "../../../components/Button/Button";
+
+const PostForm: React.FC = () => {
+  return (
+    <form className="form">
+      <label htmlFor="title">Title:</label>
+      <input className="form__input" type="text" id="title" required />
+      <label htmlFor="content">Content:</label>
+      <input type="text" className="form__input" id="content" required />
+      <label htmlFor="imageUrl">Image URL:</label>
+      <input type="url" className="form__input" id="imageUrl" required />
+      <label htmlFor="author">Author:</label>
+      <input type="text" className="form__input" id="author" required />
+      <Button type="submit" text="Create post" />
+    </form>
+  );
+};
+
+export default PostForm;
