8fc157fb48f3113a291cb2eb1478075489a7c681
Erik Riquelme <erikone2311@gmail.com>
2024-11-19 15:15:17 +0100

    Test fields and button render from PostForm

---
+++ src/post/components/PostForm/PostForm.test.tsx
@@ -0,0 +1,48 @@
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import PostForm from "./PostForm";
+
+describe("Given the PostForm component", () => {
+  describe("When it rendered", () => {
+    test("Then it should show 'Title', 'Content', 'Image URL', 'Author' fields", () => {
+      render(<PostForm />);
+
+      const titleField = screen.getByLabelText(/title/i);
+      const contentField = screen.getByLabelText(/content/i);
+      const imageUrlField = screen.getByLabelText(/image url/i);
+      const authorField = screen.getByLabelText(/author/i);
+
+      expect(titleField).toBeInTheDocument();
+      expect(contentField).toBeInTheDocument();
+      expect(imageUrlField).toBeInTheDocument();
+      expect(authorField).toBeInTheDocument();
+    });
+
+    test("Then it should show a disabled 'Create post' button", () => {
+      const createPostButtonText = /create post/i;
+
+      render(<PostForm />);
+
+      const createPostButton = screen.getByRole("button", {
+        name: createPostButtonText,
+      });
+
+      expect(createPostButton).toBeDisabled();
+    });
+  });
+
+  describe("When the user fills the title field with 'Me gustan las patatas'", () => {
+    test("Then it should show 'Me gustan las patatas'inside the field", async () => {
+      const user = userEvent.setup();
+      const titleFieldTextByUser = "Me gustan las patatas";
+
+      render(<PostForm />);
+
+      const titleField = screen.getByLabelText(/title/i);
+
+      await user.type(titleField, titleFieldTextByUser);
+
+      expect(titleField).toHaveValue(titleFieldTextByUser);
+    });
+  });
+});
