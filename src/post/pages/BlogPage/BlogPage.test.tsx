227f9413f85fbcac7fc3e0a6f5d9095942987977
Erik Riquelme <erikone2311@gmail.com>
2024-11-21 11:41:57 +0100

    Add test for BlogPage component

---
+++ src/post/pages/BlogPage/BlogPage.test.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from "@testing-library/react";
+import BlogPage from "./BlogPage";
+
+describe("Given the BlogPage component", () => {
+  describe("When rendered", () => {
+    test("Then it should show 'Blog' in a heading", () => {
+      const blogPageTitleText = /posts/i;
+
+      render(<BlogPage />);
+
+      const blogPageTitle = screen.getByRole("heading", {
+        name: blogPageTitleText,
+      });
+
+      expect(blogPageTitle).toBeInTheDocument();
+    });
+
+    test("Then it should show a loading", () => {
+      const loadingText = /loading/i;
+
+      render(<BlogPage />);
+
+      const loading = screen.getByRole("alert", {
+        name: loadingText,
+      });
+
+      expect(loading).toBeInTheDocument();
+    });
+  });
+
+  describe("When rendered and receives 'I like potatoes' and 'I hate potatoes with katchup' posts", () => {
+    test("Then it should show 'I like potatoes' and 'I hate potatoes with katchup' inside a heading", async () => {
+      const post1TitleText = /i like potatoes/i;
+      const post2TitleText = /i hate potatoes with katchup/i;
+
+      render(<BlogPage />);
+
+      const post1Title = await screen.findByRole("heading", {
+        name: post1TitleText,
+      });
+
+      const post2Title = await screen.findByRole("heading", {
+        name: post2TitleText,
+      });
+
+      expect(post1Title).toBeInTheDocument();
+      expect(post2Title).toBeInTheDocument();
+    });
+
+    test("Then it should show two images with alternatives texts 'Fried potatoes' and 'Potatoes with katchup'", async () => {
+      render(<BlogPage />);
+
+      const post1Image = await screen.findByAltText(/fried potatoes/i);
+      const post2Image = await screen.findByAltText(/potatoes with katchup/i);
+
+      expect(post1Image).toBeInTheDocument();
+      expect(post2Image).toBeInTheDocument();
+    });
+  });
+});
